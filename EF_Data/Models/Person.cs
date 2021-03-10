e62e883cff8b22ad4eb3111f781b179907aa91ec
vieriu.alexandru.25 <vieriu.alexandru.25@gmail.com>
2021-03-10 22:51:43 +0200

    55:00

--- EF_Data/Models/Person.cs
+++ EF_Data/Models/Person.cs
@@ -7,6 +7,7 @@ namespace EF_Data.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int Age { get; set; }
         public List<Address> Address { get; set; } = new List<Address>();
         public List<Email> EmailAddress { get; set; } = new List<Email>();
     }
